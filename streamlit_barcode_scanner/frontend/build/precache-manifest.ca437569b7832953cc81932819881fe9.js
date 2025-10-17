d9337d8a50207a2024e4ee094f98a21427835bbb
cbaser <c.akinbaser@hotmail.com>
2025-10-17 10:02:51 +0200

    Build added v2

---
+++ streamlit_barcode_scanner/frontend/build/precache-manifest.ca437569b7832953cc81932819881fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48688a311107cd757a9e2935c2860200",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3acb091c910859ee2d2a",
+    "url": "./static/js/2.3845d530.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.3845d530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c604c68b7cf6c0a9a2b",
+    "url": "./static/js/main.2a4d6cad.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
